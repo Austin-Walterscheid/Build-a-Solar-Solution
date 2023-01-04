39b407803508d725283a344c45c4f017b10ed46d
Austin Walterscheid <aus.10walterscheid@gmail.com>
2023-01-04 16:15:44 -0700

    first merger

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 
+
+
 function App() {
   return (
     <div className="App">
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-
+import videobg from '../videos/vid.mp4'
+import styles from "./HomeScreen.module.css"
 
 
 const HomeScreen = () => {
- return (
-    <div className=''>
+    return (
 
- <h1>Build a Solar Solution</h1>
-
-    </div>
- )
+        <div className={styles.main}>
+            <div className={styles.overlay}></div>
+            <video src={videobg} autoPlay loop muted />
+             <div className={styles.cont}>
+                <h1>Build a Solar Solution</h1>
+                <p>Solar without the salesman!</p>
+                <button>Do it Yourself</button>
+            </div>
+         </div>
+    )
 }
 
 export default HomeScreen;
