fdbc397e245fc2556b6994d9fa9d4636c1a1fcad
Kaleck Hamm <kaleckh@gmail.com>
2023-01-05 16:03:54 -0700

    fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
-import "./header.css";
+import "./header.module.css";
+import { useNavigate } from 'react-router-dom';
 export default function Header(props) {
+  const navigate = useNavigate()
     
   return (
     <header className="contactHeader">
       <div className="headerContainer">
-        <div className="headerPieces">Home</div>
-        <div className="headerPieces">About</div>
-        <div className="headerPieces">Finance Options</div>
-        <div className="headerPieces">Diy Kits</div>
-        <div className="headerPieces">Build your own kit</div>
+        <div onClick={() => {navigate()}} className="headerPieces">Home</div>
+        <div onClick={() => {navigate('/about')}} className="headerPieces">About</div>
+        <div onClick={() => navigate('/finance-options')} className="headerPieces">Finance Options</div>
+        <div onClick={() => navigate('/DIYKits')} className="headerPieces">Diy Kits</div>
+        <div onClick={() => navigate('/headerPieces')} className="headerPieces">Build your own kit</div>
       </div>
       <img src={props.picture} alt="" />
     </header>
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import videobg from '../videos/vid.mp4'
-import styles from "./HomeScreen.module.css"
-
+import React from "react";
+import videobg from "../videos/vid.mp4";
+import styles from "./HomeScreen.module.css";
+import Header from '../components/Header'
 
 const HomeScreen = () => {
-    return (
-
-        <div className={styles.main}>
-            <div className={styles.overlay}></div>
-            <video src={videobg} autoPlay loop muted />
-             <div className={styles.cont}>
-                <h1>Build a Solar Solution</h1>
-                <p>Solar without the salesman!</p>
-                <button>Do it Yourself</button>
-            </div>
-         </div>
-    )
-}
+  return (
+    <div>
+      <div className={styles.main}>
+        <div className={styles.overlay}></div>
+        <video src={videobg} autoPlay loop muted />
+        <div className={styles.cont}>
+          <Header/>
+          <h1>Build a Solar Solution</h1>
+          <p>Solar without the salesman!</p>
+          <button>Do it Yourself</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomeScreen;
