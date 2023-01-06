9e57aaba9840b20ebcee9d497f02907d546c081e
Austin Walterscheid <aus.10walterscheid@gmail.com>
2023-01-06 12:19:29 -0700

    attempt to merge header

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
-import videobg from '../videos/vid.mp4'
-import styles from "./HomeScreen.module.css"
+import videobg from '../media/vid.mp4'
+import styles from "./homescreen.module.css"
 
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
     return (
-
+        <div>
+      <header className="contactHeader">
+      <div className="headerContainer">
+        <div className="headerPieces">Home</div>
+        <div className="headerPieces">About</div>
+        <div className="headerPieces">Finance Options</div>
+        <div className="headerPieces">Diy Kits</div>
+        <div className="headerPieces">Build your own kit</div>
+      </div>
+      <img src={props.picture} alt="" />
+    </header>
         <div className={styles.main}>
             <div className={styles.overlay}></div>
             <video src={videobg} autoPlay loop muted />
-             <div className={styles.cont}>
+             <div className={styles.contain}>
                 <h1>Build a Solar Solution</h1>
                 <p>Solar without the salesman!</p>
                 <button>Do it Yourself</button>
-            </div>
+            </div>g
+         </div>
          </div>
     )
 }
