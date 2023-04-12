effc74af654b04ef5ac88c987e9bdbab514b8af9
Austin Walterscheid <aus.10walterscheid@gmail.com>
2023-04-12 13:07:00 -0600

    changed start button to start from do it yourself

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -73,7 +73,7 @@ const HomeScreen = () => {
                   navigate("/diy");
                 }}
               >
-                Do it Yourself
+                Start
               </button>
             </div>
           </div>
